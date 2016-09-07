2dec9a45126f6b7453482bf59f7e8f8c0d4d51f8
tqcenglish <tqcenglish@gmail.com>
2016-09-07 17:45:09 +0800

    update test

--- asterisk/main.go
+++ asterisk/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/tqcenglish/ini"
+	"github.com/tqcenglish/ini/asterisk/models"
+)
 
 func main() {
 	fmt.Print("hello world!")
+	cfg, err := ini.Load("../testdata/users_callcenter.conf")
+	if err != nil {
+		panic(err)
+	}
+	p := new(models.Extension)
+	err = cfg.Section("8702").MapTo(p)
+	if err != nil {
+		fmt.Print(err)
+	}
+	fmt.Print(p)
+
+	fmt.Printf("\nsection 8702 keys %v\n", cfg.Section("8702").KeyStrings())
+	fmt.Printf("section sipCallCenter keys %v\n", cfg.Section("sipCallCenter").KeyStrings())
 }
--- asterisk/models/extensions_test.go
+++ asterisk/models/extensions_test.go
@@ -4,18 +4,21 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/ini"
+	"github.com/tqcenglish/ini"
+	"github.com/tqcenglish/ini/asterisk/models"
 )
 
-func Test_Section(t *testing.T) {
+func Test_Extension(t *testing.T) {
 	cfg, err := ini.Load("../../testdata/users_callcenter.conf")
 	if err != nil {
 		panic(err)
 	}
-	p := new(Extension)
+	p := new(models.Extension)
 	err = cfg.Section("8702").MapTo(p)
 	if err != nil {
 		fmt.Print(err)
 	}
 	fmt.Print(p)
+
+	fmt.Printf("section 8702 keys %v", cfg.Section("8702").Keys())
 }
